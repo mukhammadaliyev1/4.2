6d748c6589390f785eff7e1fd09d448d1a701d74
mukhammadaliyev1 <muhammadnurmamadaliyev335@gmail.com>
2024-06-06 08:35:36 +0500

    z

---
+++ index.js
@@ -0,0 +1,97 @@
+//1-masala
+// let num = +prompt("3 xonali son kiriting");
+// let yuzlar = Math.trunc(num / 100);
+// console.log(yuzlar);
+
+//2-masala
+// let num = +prompt("3 xonali son kiriting");
+// let yuzlar = Math.trunc(num / 100);
+// let b = Math.trunc(num % 10);
+// let o = yuzlar % 10;
+// console.log(b, o);
+
+// 3-masala
+
+// let num = +prompt("3 xonali son kiriting");
+// let y = Math.trunc(num / 100);
+// let o = yuzlar % 10;
+// let b = Math.trunc(num % 10);
+// let res = b + "" + o + "" + y;
+// console.log(res);
+
+// 4-masala
+// let a = +prompt("Uch xonali son kiriting");
+// let son = a % 100;
+// let birlik = son % 10;
+// let onlik = son / 10;
+// let yuzlik = a / 100;
+// let i = birlik * 100 + onlik * 10 + yuzlik;
+// console.log(i);
+
+//5-masala
+// let a = +prompt("Uch xonali son kiriting");
+// let son = a % 100;
+// let birlik = son % 10;
+// let onlik = son / 10;
+// let yuzlik = a / 100;
+// let i = onlik * 100 + yuzlik * 10 +  birlik;
+// console.log(i);
+
+// 6-masala
+// let num = +prompt("Uch xonali son kiriting");
+// let onlik = num / 10;
+// let yuzlik = Math.trunc(num / 100);
+// let birlik = num % 10;
+// let i = onlik + "" + yuzlik + "" + birlik;
+
+// 7-masala
+// let num = +prompt("Uch xonali son kiriting");
+// let onlik = num / 10;
+// let yuzlik = Math.trunc(num / 100);
+// let birlik = num % 10;
+// let i =yuzlik + "" +birlik   + "" + onlik;
+
+// 8-masala
+// let num = +prompt("To`rt xonali son kiriting");
+// let b = Math.trunc(num % 100) / 10;
+// console.log(b);
+
+// 9-masala
+// let num = +prompt("To`rt xonali son kiriting");
+// let b = Math.trunc((num % 10000) / 1000);
+// console.log(b);
+
+//10-masala
+// let N = +prompt("n ni kiriting");
+// let i = N / 60;
+// console.log(i);
+
+// 11-masala
+// let N = +prompt("n ni kiriting");
+// let i = N / 3600;
+// console.log(i);
+
+// 12-masala
+// let N = +prompt("N ni kiriting");
+// let s = N / 1;
+// let m = N / 60;
+// console.log(m, s);
+
+// 13-masala
+// let N = +prompt("N ni kiriting");
+// let i = N / 60;
+// let s = N / 1;
+// console.log(i, s);
+
+// 14-masala
+// let N = +prompt("N ni kiriting");
+// let soat = N / 3600;
+// let s = N / 1;
+// console.log(soat, s);
+
+// 15-masala
+// let N = +prompt("N ni kiriting");
+// let soat = N / 3600;
+// let s = N / 1;
+// let m = N / 60;
+// console.log(soat, s, m);
